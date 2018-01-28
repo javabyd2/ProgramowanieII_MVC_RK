94d3b027fcf638b1487812b556bdb14dbde214a5
Radek Kryszewski <radoslaw246@wp.pl>
2018-01-28 12:09:11 +0100

    initial project MVC

--- src/main/java/com/sdabyd2/programowanie/Controller/GiantController.java
+++ src/main/java/com/sdabyd2/programowanie/Controller/GiantController.java
@@ -6,46 +6,49 @@ import com.sdabyd2.programowanie.Model.Health;
 import com.sdabyd2.programowanie.Model.Nourishment;
 import com.sdabyd2.programowanie.View.GiantView;
 
+import java.util.List;
+
 public class GiantController { //odpowiedzialna za ustawianie
 
-    private GiantModel giantModel;
+    private List<GiantModel> giantModel;
     private GiantView giantView;
 
-    public GiantController(GiantModel giantModel, GiantView giantView) {
+    public GiantController(List<GiantModel> giantModel, GiantView giantView) {
 
         this.giantModel = giantModel;
         this.giantView = giantView;
 
     }
 
-    public Health getHealth() {
-        return giantModel.getHealth();  //każdy potworek może mieć swoje zdrowie
+    public Health getHealth(int k) {
+        return giantModel.get(k).getHealth();  //każdy potworek może mieć swoje zdrowie
     }
 
-    public void setHealth(Health health) {
-        this.giantModel.setHealth(health);
+    public void setHealth(Health health, int k) {
+        this.giantModel.get(k).setHealth(health);
 
     }
 
-    public Fatigue getFatigue(){
-        return giantModel.getFatigue();
+    public Fatigue getFatigue(int k){
+        return giantModel.get(k).getFatigue();
     }
 
-    public void setFatigue(Fatigue fatigue){
-        this.giantModel.setFatigue(fatigue);
+    public void setFatigue(Fatigue fatigue, int k){
+        this.giantModel.get(k).setFatigue(fatigue);
     }
 
-    public Nourishment getNourishment(){
-        return giantModel.getNourishment();
+    public Nourishment getNourishment(int k){
+        return giantModel.get(k).getNourishment();
     }
 
-    public void setNourishment (Nourishment nourishment){
-        this.giantModel.setNourishment(nourishment);
+    public void setNourishment (Nourishment nourishment, int k){
+        this.giantModel.get(k).setNourishment(nourishment);
     }
 
     public void updateView(){
-        //metoda
-        this.giantView.displayGiant(giantModel);
+        for(GiantModel model: giantModel)
+
+        this.giantView.displayGiant(model);
 
     }
 
--- src/main/java/com/sdabyd2/programowanie/Main.java
+++ src/main/java/com/sdabyd2/programowanie/Main.java
@@ -1,32 +1,40 @@
 package com.sdabyd2.programowanie;
 
-        import com.sdabyd2.programowanie.Controller.GiantController;
-        import com.sdabyd2.programowanie.Model.Fatigue;
-        import com.sdabyd2.programowanie.Model.GiantModel;
-        import com.sdabyd2.programowanie.Model.Health;
-        import com.sdabyd2.programowanie.Model.Nourishment;
-        import com.sdabyd2.programowanie.View.GiantView;
+import com.sdabyd2.programowanie.Controller.GiantController;
+import com.sdabyd2.programowanie.Model.Fatigue;
+import com.sdabyd2.programowanie.Model.GiantModel;
+import com.sdabyd2.programowanie.Model.Health;
+import com.sdabyd2.programowanie.Model.Nourishment;
+import com.sdabyd2.programowanie.View.GiantView;
 
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        GiantModel giant1 = new GiantModel(Health.HEALTH, Fatigue.NO, Nourishment.NOTHUNGRY);
+        List<GiantModel> lista = new ArrayList<>();
+
+        GiantModel ghost = new GiantModel(Health.HEALTH, Fatigue.NO, Nourishment.NOTHUNGRY);
+        lista.add(ghost);
         GiantModel frankestein = new GiantModel(Health.HEALTH, Fatigue.YES, Nourishment.STRAVING);
+        lista.add(frankestein);
+        GiantModel zoombie = new GiantModel(Health.DEAD, Fatigue.NO, Nourishment.HUNGRY);
+        lista.add(zoombie);
+        GiantModel skeleton = new GiantModel(Health.NOTHEALTHY, Fatigue.SLEEPING, Nourishment.HUNGRY);
+        lista.add(skeleton);
 
-      
-
-        GiantView view = new GiantView();
-        GiantController controller = new GiantController(frankestein, view);
-
-      controller.updateView();
-
-      controller.setHealth(Health.DEAD);
-      controller.updateView();
+        GiantController giantController = new GiantController(lista, new GiantView());
 
 
+        //GiantView view = new GiantView();
+        //GiantController controller = new GiantController(frankestein, view);
+
+        giantController.updateView();
+
+        //controller.setHealth(Health.DEAD);
+        // controller.updateView();
 
 
     }
